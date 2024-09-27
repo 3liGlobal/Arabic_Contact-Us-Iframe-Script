53e686908e255c2617628e97db10a7cdb6b8ceb7
HoorUlAin-3liGlobal <hoor.ulain@3li.global>
2024-09-27 17:37:10 +0500

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -1,3 +1,27 @@
+function initializeEnglishIframe() {
+    const iframe = document.getElementById('iframeContactUsOOKAUAE');
+    if (!iframe) return;
+
+    console.log("Script Started...");
+    iframe.src = 'https://3liglobal.github.io/Contact_Us-Form_OOKA_UAE';
+    let email;
+
+    const checkEmailInterval = setInterval(function () {
+        const divElement = document.getElementById("swell-customer-identification");
+        email = divElement.hasAttribute("data-email") ? divElement.getAttribute("data-email") : null;
+
+        if (email) {
+            console.log("Email found: " + email);
+            // iframe.contentWindow.postMessage(email, "*");
+            iframe.src = `https://3liglobal.github.io/Contact_Us-Form_OOKA_UAE?email=${encodeURIComponent(email)}`;
+            clearInterval(checkEmailInterval);
+        } else {
+            console.log("No email found English, using default URL.");
+        }
+    }, 500);
+}
+
+
 function initializeArabicIframe() {
     const iframe = document.getElementById('iframeContactUsOOKAUAEArabic');
     if (!iframe) return;
@@ -16,7 +40,7 @@ function initializeArabicIframe() {
             iframe.src = `https://3liglobal.github.io/Arabic_Contact_Us-Form_OOKA_UAE?email=${encodeURIComponent(email)}`;
             clearInterval(checkEmailInterval);
         } else {
-            console.log("No email found, using default URL.");
+            console.log("No email found Arabic, using default URL.");
         }
     }, 500);
 }
@@ -29,8 +53,14 @@ const arabicObserver = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
         const newLanguageButton = document.querySelector('.language-switcher_button');
         if (newLanguageButton) {
-            //window.location.reload();
-            newLanguageButton.addEventListener('click', initializeArabicIframe);
+            var language =  document.querySelector('.language-switcher_button').children[0].innerHTML;
+                if(language!='English'){
+                     newLanguageButton.addEventListener('click', initializeArabicIframe);
+                }
+                //window.location.reload();
+               else{ 
+                   newLanguageButton.addEventListener('click', initializeEnglishIframe);
+               }
         }
     });
 });
