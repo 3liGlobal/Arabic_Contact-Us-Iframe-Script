7721b7542fdbc28f62d0ae03c2a1dc488cc7d276
HoorUlAin-3liGlobal <hoor.ulain@3li.global>
2024-09-27 18:07:36 +0500

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -45,14 +45,11 @@ function initializeArabicIframe() {
     }, 500);
 }
  
-var language =  document.querySelector('.language-switcher_button').children[0].innerHTML;
-                if(language!='English'){
+
                      newLanguageButton.addEventListener('click', initializeArabicIframe);
-                }
+               
                 //window.location.reload();
-               else{ 
-                   newLanguageButton.addEventListener('click', initializeEnglishIframe);
-               }
+              
  
 // MutationObserver for Arabic iframe
 const arabicObserver = new MutationObserver((mutations) => {
